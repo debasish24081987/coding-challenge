c94930d987ef2bb9f424907e9c388d1dfc1639d3
debasish mahapatra <debasish.shiney@gmail.com>
2021-03-23 23:27:17 +0530

    Commit of reverse string

--- csharp/src/ReversingString/StringUtilities.cs
+++ csharp/src/ReversingString/StringUtilities.cs
@@ -4,9 +4,24 @@ namespace CodingChallenge.ReversingString
 {
     public class StringUtilities
     {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string : ");
+            string text = Console.ReadLine();
+            Console.Write("Reverse String : ");
+            Console.WriteLine(Reverse(text));
+        }
+
         public static string Reverse(string s)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(s) || s.Length == 1)
+            {
+                return s;
+            }
+
+            var myArr = s.ToCharArray();
+            Array.Reverse(myArr);
+            return new string(myArr);
         }
     }
 }
